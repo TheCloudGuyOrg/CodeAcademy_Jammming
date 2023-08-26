fdf485375dee32ad1d2065f42dea7653854723cb
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-08-26 10:41:48 -0600

    Building App.js and App.module.css scaffoding

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, {useState, useCallback} from "react";
+import './App.module.css';
+
+// import Playlist from "../Playlist/Playlist";
+// import SearchBar from "../SearchBar/SearchBar";
+// import SearchResults from "../SearchResults/SearchResults";
+// import Spotify from "../../util/Spotify";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        {/* <SearchBar/>
+        <div className="App-playlist">
+          <SearchResults/>
+          <Playlist/>
+        </div> */}
+      </div>
     </div>
   );
 }
