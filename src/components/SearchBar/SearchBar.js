65f18365a20a47615cf6b71f68cd0a293d054d06
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-08-28 08:59:43 -0600

    Correct Spelling for Search Bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,7 @@ import styles from './SearchBar.module.css';
 function SearchBar() {
     return (
         <div className={styles.SearchBar}>
-          <input placeholder="Enter A Song, Albuim, or Artist"/>
+          <input placeholder="Enter A Song, Album, or Artist"/>
           <button className={styles.SearchButton}>
             SEARCH
           </button>
