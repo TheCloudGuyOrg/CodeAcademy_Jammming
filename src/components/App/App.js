6769a970d05a1b65c8f0bafa5f21756f8e2f5814
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-08-28 08:39:50 -0600

    Fixed App Styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,19 @@
 // import React, {useState, useCallback} from "react";
-import './App.module.css';
+import styles from './App.module.css';
 
 import Playlist from "../Playlist/Playlist";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 
-
 function App() {
   return (
     <div>
       <h1>
-        Ja<span className="highlight">mmm</span>ing
+        Ja<span className={styles.highlight}>mmm</span>ing
       </h1>
-      <div className="App">
+      <div className={styles.App}>
         <SearchBar/>
-        <div className="App-playlist">
+        <div className={styles.AppPlaylist}>
           <SearchResults/>
           <Playlist/>
         </div>
