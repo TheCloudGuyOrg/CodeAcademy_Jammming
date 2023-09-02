4b438726461732b8e2ec0b91f2d1f2a22d40067e
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-09-02 09:33:48 -0600

    Reset env

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,43 +1,37 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: '../../.env' });
-const SESSION_SECRET = process.env.SESSION_SECRET;
+const jsonResponse = [
+    {
+      artist: 'Rutimental',
+      album: '2023 Essential Mix',
+      name: 'Rutimental 2023 Essential Mix',
+      id: '1',
+      uri: 'www.spotify.com/rutimental'
+    },
+    {
+      artist: 'Chase and Status',
+      album: '2021 Essential Mix',
+      name: 'Chase and Status 2021 Essential Mix',
+      id: '2',
+      uri: 'www.spotify.com/chaseandstatus'
+    },
+    {
+      artist: 'Subfocus',
+      album: '2022 Essential Mix',
+      name: 'Subfocus 2022 Essential Mix',
+      id: '3',
+      uri: 'www.spotify.com/subfocus'
+    }
+  ]
 
-console.log(SESSION_SECRET)
+const Spotify = {
+    search(term) {
+        return jsonResponse.map(track => ({
+            id: track.id,
+            name: track.name,
+            artist: track.artist,
+            album: track.album,
+            uri: track.uri
+        }))
+    }
+  }
 
-// const jsonResponse = [
-//     {
-//       artist: 'Rutimental',
-//       album: '2023 Essential Mix',
-//       name: 'Rutimental 2023 Essential Mix',
-//       id: '1',
-//       uri: 'www.spotify.com/rutimental'
-//     },
-//     {
-//       artist: 'Chase and Status',
-//       album: '2021 Essential Mix',
-//       name: 'Chase and Status 2021 Essential Mix',
-//       id: '2',
-//       uri: 'www.spotify.com/chaseandstatus'
-//     },
-//     {
-//       artist: 'Subfocus',
-//       album: '2022 Essential Mix',
-//       name: 'Subfocus 2022 Essential Mix',
-//       id: '3',
-//       uri: 'www.spotify.com/subfocus'
-//     }
-//   ]
-
-// const Spotify = {
-//     search(term) {
-//         return jsonResponse.map(track => ({
-//             id: track.id,
-//             name: track.name,
-//             artist: track.artist,
-//             album: track.album,
-//             uri: track.uri
-//         }))
-//     }
-//   }
-
-//   export default Spotify;
+  export default Spotify;
